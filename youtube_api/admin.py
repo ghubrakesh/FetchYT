2fafaa543935c9a3100b53d21e5297a5cbd9733b
rakeshateka <102187286+ghubrakesh@users.noreply.github.com>
2024-10-24 13:46:55 +0530

    added filter option for admin panel

--- youtube_api/admin.py
+++ youtube_api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Video
 
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id', 'publish_datetime']
+    list_filter = ['publish_datetime']
 
-admin.site.register(Video)
+admin.site.register(Video, VideoAdmin)
